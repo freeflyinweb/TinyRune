10c426f47aa775d238aa43dcf65fef1d5d5cb6f7
freeflyinweb <freeflyinweb@gmail.com>
2013-03-04 20:50:31 -0700

    Added initial config along with button. Added camera button which currently saves screenshot.png to the directory in which your jar is in.

--- src/org/tinyrune/Client.java
+++ src/org/tinyrune/Client.java
@@ -2,12 +2,20 @@ package org.tinyrune;
 
 import org.tinyrune.canvas.LoaderCanvas;
 import org.tinyrune.rs.RunescapeClient;
+import org.tinyrune.ui.ConfigButton;
+import org.tinyrune.ui.ScreenShotButton;
+import org.tinyrune.util.Settings;
 
 import javax.swing.*;
 import java.applet.Applet;
 import java.awt.*;
+import java.io.File;
 
 public class Client extends JFrame {
+
+    private JPanel game;
+    private LoaderCanvas canvas;
+
     public static void main(String[] args) {
         new Client();
     }
@@ -17,10 +25,23 @@ public class Client extends JFrame {
         this.setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setResizable(false);
-        JPanel game = new JPanel(new BorderLayout());
-        LoaderCanvas canvas = new LoaderCanvas();
-        canvas.setText("Loading Runescape... Please Wait");
-        game.add(canvas, BorderLayout.CENTER);
+        Settings.read();
+        this.game = new JPanel(new BorderLayout());
+        canvas = new LoaderCanvas();
+        canvas.setText("Runescape Downloading... Please Wait");
+
+        JToolBar toolbar = new JToolBar();
+        ConfigButton configButton = new ConfigButton();
+        ScreenShotButton screenShotButton = new ScreenShotButton(this);
+
+        toolbar.add(configButton);
+        toolbar.add(screenShotButton);
+
+        configButton.addActionListener(configButton);
+        screenShotButton.addActionListener(screenShotButton);
+
+        this.game.add(canvas, BorderLayout.CENTER);
+        this.getContentPane().add(toolbar, BorderLayout.PAGE_START);
         this.getContentPane().add(game, BorderLayout.CENTER);
         this.pack();
         this.setVisible(true);
@@ -33,6 +54,10 @@ public class Client extends JFrame {
         game.remove(canvas);
         game.add(applet, BorderLayout.CENTER);
         this.pack();
+    }
+
+    public LoaderCanvas getCanvas() {
+        return this.canvas;
     }
 
 }
--- src/org/tinyrune/rs/RunescapeClient.java
+++ src/org/tinyrune/rs/RunescapeClient.java
@@ -1,6 +1,7 @@
 package org.tinyrune.rs;
 
-import org.tinyrune.io.RunescapePageCrawler;
+import org.tinyrune.net.RunescapePageCrawler;
+import org.tinyrune.util.Settings;
 import java.applet.Applet;
 import java.applet.AppletContext;
 import java.applet.AppletStub;
@@ -16,20 +17,20 @@ public class RunescapeClient implements AppletStub {
 
     public RunescapeClient() {
         try {
-            this.crawler = new RunescapePageCrawler(new URL("http://oldschool45.runescape.com"));
+            this.crawler = new RunescapePageCrawler();
             this.crawler.crawl();
-            URLClassLoader classLoader = new URLClassLoader(new URL[]{new URL("http://oldschool45.runescape.com/" + this.crawler.getGameBase())});
+            URLClassLoader classLoader = new URLClassLoader(new URL[]{new URL(Settings.get("url") + crawler.getGameBase())});
             Class<?> clientClass = classLoader.loadClass("client");
             this.applet = (Applet)clientClass.newInstance();
             this.applet.setStub(this);
             this.applet.setPreferredSize(new Dimension(765, 503));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         }
     }
@@ -46,7 +47,7 @@ public class RunescapeClient implements AppletStub {
     @Override
     public URL getDocumentBase() {
         try {
-            return new URL("http://oldschool45.runescape.com");
+            return new URL(Settings.get("url"));
         } catch (MalformedURLException e) {
             e.printStackTrace();
             return null;
@@ -56,7 +57,7 @@ public class RunescapeClient implements AppletStub {
     @Override
     public URL getCodeBase() {
         try {
-            return new URL("http://oldschool45.runescape.com");
+            return new URL(Settings.get("url"));
         } catch (MalformedURLException e) {
             e.printStackTrace();
             return null;
